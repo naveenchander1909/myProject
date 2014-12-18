a87315640d7d1b01025883e39f309984c1859062
amit-sf <amit.sharma@sourcefuse.com>
2014-12-18 13:20:37 +0530

    sdxsd

--- join.php
+++ join.php
@@ -5,3 +5,4 @@ $db->connect();
 $db->select('CRUDClass','CRUDClass.id,CRUDClass.name,CRUDClassChild.name','CRUDClassChild ON CRUDClass.id = parentId','CRUDClass.name="Name 1"','id DESC'); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
 $res = $db->getResult();
 print_r($res);
+echo "New Changes";
